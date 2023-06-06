5ff492e4907af1af5ebde4fd758cbacb170570dd
Ankit TechyTrion <134504487+annidesign@users.noreply.github.com>
2023-06-06 10:56:18 +0530

    functions.php

--- functions.php
+++ functions.php
@@ -31,3 +31,21 @@ function wpb_custom_new_menu() {
 }
 add_action( 'init', 'wpb_custom_new_menu' );
 
+
+// Register Sidebars
+function custom_sidebars() {
+ 
+    $args = array(
+        'id'            => 'custom_sidebar',
+        'name'          => __( 'Custom Widget Area', 'text_domain' ),
+        'description'   => __( 'A custom widget area', 'text_domain' ),
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+    );
+    register_sidebar( $args );
+ 
+}
+add_action( 'widgets_init', 'custom_sidebars' );
+
