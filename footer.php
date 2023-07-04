da3dea6311c7021a9ee37a881a89eced20b37164
Ankit TechyTrion <134504487+annidesign@users.noreply.github.com>
2023-07-04 11:01:17 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+	/* The footer widget area is triggered if any of the areas 
+* have widgets. So let's check that first. 
+* 
+* If none of the sidebars have widgets, then let's bail early. 
+*/
+	if (   ! is_active_sidebar( 'first-footer-widget-area'  )
+		&& ! is_active_sidebar( 'second-footer-widget-area' )
+		&& ! is_active_sidebar( 'third-footer-widget-area'  )
+		&& ! is_active_sidebar( 'fourth-footer-widget-area' )
+	)
+		return;
+	//end of all sidebar checks. 
+	
+    ?>
+<?php wp_footer(); ?>
+
+</body>
+</html>
